19494a7eeb09b09c41fd9322690714ac57ed9353
Danny Martinez <iamdannyae@gmail.com>
2023-04-20 18:51:33 -0400

    finished fixing index.js after i lost the original file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = require("./connection")
 //     user: "root",
 //     password: "nineteen99",
 //     database: "employees_db"
+
 // })
 
 function main(){
@@ -24,13 +25,10 @@ function main(){
                 "add a department",
                 "add a role",
                 "add an employee",
-                "update an employee role",
-
-
-
-         
+                "update an employee role",         
         ]
     }])
+
     .then((choice)=>{
         console.log(choice)
 
@@ -39,14 +37,27 @@ function main(){
                 viewAllDepartment()
                 break
             case 'view all roles':
+                viewAllRoles()
+                break
             case 'view all employees':
+                viewAllEmployees()
+                break
             case 'add a department':
+                addDepartment()
+                break
             case 'add a role':
+                addRole()
+                break
             case 'add an employee':
+                addEmployee()
+                break
             case 'update an employee role':
-            
-              break;
+                updateEmployeeRole()
+                break
+
             default:
+                console.log("something went wrong")
+                break
              
           }
     })
@@ -54,8 +65,132 @@ function main(){
 }
 function viewAllDepartment(){
 db.promise().query("SELECT * FROM department").then(data=>{
-    console.table(data)
+    console.table(data[0])
+    main()
 })
+}
+
+function viewAllRoles(){
+    db.promise().query("SELECT * FROM role").then(data=>{
+        console.table(data[0])
+        main()
+    })
+    }
+
+function viewAllEmployees(){
+    db.promise().query("SELECT * FROM employee").then(data=>{
+        console.table(data[0])
+        main()
+    })
+    }
+
+function addDepartment(){
+    inquirer.prompt([{
+        type: "input",
+        name: "department",
+        message: "what department wouldyou like to add?",
+
+    }])
+    .then(async answer => {
+        await db.promise().query(`insert into department (name) values ('${answer.department}')`)
+        viewAllDepartment()
+    })
+}
+
+
+async function addRole(){
+    const [departments]= await db.promise().query("SELECT * FROM department")
+    inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "what title would you like to add?",
+    
+
+    },{
+        type: "input",
+        name: "salary",
+        message: "what is the salary for this role?",
+    },{
+        type: "list",
+        name: "department_id",
+        message: "Please select department for this role",
+        choices: departments.map(({name,id})=>({
+        name,
+        value: id
+        }))
+
+    }])
+    .then(async answer => {
+        await db.promise().query(`insert into role (title,salary,department_id) values ('${answer.title}',${answer.salary}, ${answer.department_id})`)
+        viewAllRoles()
+    })
+}
+
+async function addEmployee(){
+    const [roles]= await db.promise().query("SELECT * FROM role")
+    const [employees]= await db.promise().query("SELECT * FROM employee")
+    inquirer.prompt([{
+        type: "input",
+        name: "first_name",
+        message: "what is the first name?",
+    
+
+    },{
+        type: "input",
+        name: "last_name",
+        message: "what is the last name?",
+    },{
+        type: "list",
+        name: "role_id",
+        message: "Please select department for this role",
+        choices: roles.map(({title,id})=>({
+        name: title, 
+        value: id
+        }))
+
+    },{
+        type: "list",
+        name: "manager_id",
+        message: "Who is the manager for this employee?",
+        choices: employees.map(({first_name, last_name, id})=>({
+        name: first_name + " " + last_name,
+        value: id
+        }))
+    }])
+    .then(async answer => {
+        await db.promise().query(`insert into employee (first_name, last_name, role_id, manager_id) values ('${answer.first_name}','${answer.last_name}', ${answer.role_id}, ${answer.manager_id})`)
+        viewAllEmployees()
+    })
+}
+
+async function updateEmployeeRole(){
+    const [roles]= await db.promise().query("SELECT * FROM role")
+    const [employees]= await db.promise().query("SELECT * FROM employee")
+    inquirer.prompt([{
+        type: "list",
+        name: "employee",
+        message: "please choose and employee to update?",
+        choices: employees.map(({first_name, last_name, id})=>({
+            name: first_name + " " + last_name,
+            value: id
+            }))
+    
+    },
+       
+    {
+        type: "list",
+        name: "role_id",
+        message: "Please choose new role",
+        choices: roles.map(({title,id})=>({
+        name: title, 
+        value: id
+        }))
+
+    }])
+    .then(async answer => {
+        await db.promise().query(`update employee set role_id = ${answer.role_id} where id= ${answer.employee}`)
+        viewAllEmployees()
+    })
 }
 
 main()
